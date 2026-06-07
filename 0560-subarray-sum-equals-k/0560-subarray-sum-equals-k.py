9091f4181286fdb5694234a566ae2c071ceacfdd
ANGEL KUMARI <01angelkumari@gmail.com>
2026-06-07 16:57:02 +0530

    Time: 40 ms (26.79%), Space: 24 MB (6.21%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,14 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        curr_sum=0
-        prefix_count=defaultdict(int)
-        count=0
-        prefix_count[0]=1
+        curr_sum = 0
+        prefix_count = defaultdict(int)
+        count = 0
+        prefix_count[0] = 1
         for num in nums:
-            curr_sum+=num
-            count+=prefix_count[curr_sum-k] #total number of curr-k in hashmap
-            prefix_count[curr_sum]+=1
+            curr_sum += num
+            count += prefix_count[curr_sum - k]  # total number of curr-k in hashmap
+            prefix_count[curr_sum] += 1
         return count
-
-            
-        
