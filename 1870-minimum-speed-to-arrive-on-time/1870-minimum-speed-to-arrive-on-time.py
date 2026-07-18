0a2e4b4e6f6596c9df58f7a89767c05bd420408f
ANGEL KUMARI <01angelkumari@gmail.com>
2026-07-19 05:27:56 +0530

    Time: 1183 ms (83.04%), Space: 33.9 MB (13.82%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -1,7 +1,9 @@
 class Solution:
     def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
 
-        if hour <= len(dist) - 1:  #impposible, agr hour jda hoga total element in distance array,then wait time increases as each have to wait for next hour
+        if hour <= len(dist) - 1:  # Impossible case:
+# Even with infinite speed, we need more than (n-1) hours
+# because we must wait after every train except the last one.
             return -1
         left,right=1,10**7
         while left<right:
