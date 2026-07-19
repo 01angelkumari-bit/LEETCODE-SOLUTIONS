5feb3fba0ca79acd94d9feb589e536ab0cc2ac39
ANGEL KUMARI <01angelkumari@gmail.com>
2026-07-19 07:06:52 +0530

    Time: 3 ms (70.69%), Space: 19.3 MB (46.47%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+
+        left = max(nums)
+        right = sum(nums)
+
+        while left < right:
+
+            mid = (left + right) // 2
+
+            subarrays = 1  #min itna hoga hi
+            current_sum = 0
+
+            for num in nums:
+
+                if current_sum + num <= mid:
+                    current_sum += num
+                else:
+                    subarrays += 1
+                    current_sum = num
+
+            if subarrays <= k:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
